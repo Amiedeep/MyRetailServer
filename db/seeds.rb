fa0cc59e8dab409759c39b048c254b85372ed714
ajitsingh <jeetsingh.ajit@gmail.com>
2015-05-07 23:03:52 +0530

    correcting seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ Item.create(id: 1, name: 'TV', detail: 'lcd tv', image:  image_to_encoded_string
 Item.create(id: 2, name: 'Microwave', detail: 'microwave', image:  image_to_encoded_string('./public/images/microwave.jpeg'), price:4000, category_id: 10)
 
 Item.create(id: 3, name: 'Chair', detail: 'Chair', image:  image_to_encoded_string('./public/images/chair.jpeg'), price:1000, category_id: 20)
-Item.create(id: 4, name: 'Table', detail: 'Table', image:  image_to_encoded_string('./public/images/table.jpeg'), price:5000, category_id: 30)
+Item.create(id: 4, name: 'Table', detail: 'Table', image:  image_to_encoded_string('./public/images/table.jpeg'), price:5000, category_id: 20)
 
 Item.create(id: 5, name: 'T-Shirt', detail: 't-shirt', image:  image_to_encoded_string('./public/images/t_shirt.jpeg'), price:1500, category_id: 30)
 Item.create(id: 6, name: 'Jeans', detail: 'jeans', image:  image_to_encoded_string('./public/images/jeans.jpeg'), price:800, category_id: 30)
